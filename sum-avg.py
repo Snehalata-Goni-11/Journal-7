06bf3870a10cbbe92d6cb9502e4c091daf6d0f7e
Snehalata-Goni-11 <snehagoni60@gmail.com>
2025-11-27 17:19:02 +0530

    Update sum-avg.py

--- sum-avg.py
+++ sum-avg.py
@@ -7,11 +7,7 @@ else:
     print("No input given - using default scores:")
 total = sum(scores)
 average = total / len(scores)
-maximum = max(scores)
-minimum = min(scores)
 
 print("Scores =", scores)
 print("Sum =", total)
 print("Average =", average)
-print("Maximum =", maximum)
-print("Minimum =", minimum)
